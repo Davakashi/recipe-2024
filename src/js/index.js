5e6aae3d1b30463670bdeaa5e99860959b18871c
Davakashi <daavka.desu@gmail.com>
2024-02-09 02:54:10 +0800

    recipe model iig biciv

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ require("@babel/polyfill");
 import Search from "./model/search";
 import { elements, renderLoader, clearLoader } from "./view/base";
 import * as searchView from "./view/searchView";
+import Recipe from "./model/recipe";
 
 /**
  *  Web app төлөв
@@ -37,4 +38,14 @@ const controlSearch = async () => {
 elements.searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
     controlSearch();
+});
+
+elements.pageButtons.addEventListener("click", (e) => {
+    const btn = e.target.closest(".btn-inline");
+
+    if (btn) {
+        const gotoPageNumber = parseInt(btn.dataset.goto, 10);
+        searchView.clearSearchResult();
+        searchView.renderRecipes(state.search.result, gotoPageNumber);
+    }
 });
--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -22,8 +22,47 @@ export const clearSearch = () => {
 
 export const clearSearchResult = () => {
     elements.searchResultList.innerHTML = "";
+    elements.pageButtons.innerHTML = "";
 };
 export const getInput = () => elements.searchInput.value;
-export const renderRecipes = (recipes) => {
-    recipes.forEach(renderRecipe);
+export const renderRecipes = (recipes, page = 1, resPerPage = 10) => {
+    // хайлтын үр дүнг хуудаслаж үзүүлэх.
+    const start = (page - 1) * resPerPage;
+    const end = page * resPerPage;
+
+    recipes.slice(start, end).forEach(renderRecipe);
+
+    // хуудаслалтын товчуудийг гаргаж ирэх.
+    const totalPages = Math.ceil(recipes.length / resPerPage);
+    renderButtons(page, totalPages);
+};
+
+// type ====> "prev" or "next"
+const createButton = (
+    page,
+    type,
+    direction
+) => `<button class="btn-inline results__btn--${type}" data-goto=${page}>
+                    <svg class="search__icon">
+                        <use href="img/icons.svg#icon-triangle-${direction}"></use>
+                    </svg>
+                    <span>Хуудас ${page}</span>
+                </button>`;
+
+const renderButtons = (page, totalPages) => {
+    let button;
+
+    if (page === 1 && totalPages > 1) {
+        // 1-р хуудсан дээр байна, 2-р хуудас гэдэг товчийг дарах ёстой
+        button = createButton(2, "next", "right");
+    } else if (page < totalPages) {
+        // Өмнөх болон дараачийн хуудас руу шилжих товчуудийг гаргана.
+        button = createButton(page - 1, "prev", "left");
+        button += createButton(page + 1, "next", "right");
+    } else if (page === totalPages) {
+        // Хамгийн сүүлийн хуудсан дээр байна. Өмнөх рүү шилжүүлэх товчийг харуулна.
+        button = createButton(page - 1, "prev", "left");
+    }
+
+    elements.pageButtons.insertAdjacentHTML("afterbegin", button);
 };
