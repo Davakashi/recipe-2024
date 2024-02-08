bde0a73f5925a5ad1b7beab4f77cdacd8e772ea3
Davakashi <daavka.desu@gmail.com>
2024-02-09 01:39:41 +0800

    L

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,40 @@
 require("@babel/polyfill");
 import Search from "./model/search";
+import { elements, renderLoader, clearLoader } from "./view/base";
+import * as searchView from "./view/searchView";
 
-let search = new Search("pasta");
+/**
+ *  Web app төлөв
+ *  - Хайлтын query, үр дүн
+ *  - Тухайн үзүүлж байгаа жор
+ *  - Лайкалсан жорууд
+ *  - Захиалж байгаа жорын найрлагууд
+ */
 
-search.doSearch().then((r) => console.log(r));
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Вэбээс хайлтын түлхүүр үгийг гаргаж ирнэ
+    const query = searchView.getInput();
+
+    if (query) {
+        // 2) Шинээр хайлтын обектийг үүсгэж өгнө.
+        state.search = new Search(query);
+
+        // 3) Хайлт хийхэд зориулж дэлгэцийг бэлтгэнэ.
+        searchView.clearSearch();
+        searchView.clearSearchResult();
+        renderLoader(elements.searchResultDiv);
+        // 4) хайлтыг гүйцэтгэнэ.
+        await state.search.doSearch();
+        // 5) Хайлтын үр дүнг дэлгэцэнд гүйцэтгэнэ.
+        clearLoader();
+        if (state.search.result === undefined) alert("Хайлт илэрцгүй");
+        else searchView.renderRecipes(state.search.result);
+    }
+};
+
+elements.searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    controlSearch();
+});
